74c7d444c051f6d7b92b52a90135327b58b5c986
Isaac Phillips <isaacophillips@gmail.com>
2017-06-15 18:57:20 -0400

    Removed season functions from AppleTree class and moved TreeInterface to individual file.

--- program.php
+++ program.php
@@ -3,11 +3,32 @@ namespace Learning;
 
 require_once 'tree.php';
 
-$appleTree = new AppleTree;
+class Season {
+    function __construct(){
+        
+    }
+    function spring(){
+        echo "It's springtime!  ";
+        
+        echo "<br/>";
+	}
+    function summer(){
+        echo "It's summertime!  ";
+        
+        echo"<br/>";
+	}
+    function fall(){
+        echo "It's fall, time to harvest!  ";
+        
+        echo "<br/>";
+    }
+    function winter(){
+        echo "It's wintertime!  ";
+        
+        echo "<br/><br/>";
+    }
+}
 
 for ($i = 0; $i < 10; $i++){
-    $appleTree->spring();
-    $appleTree->summer();
-    $appleTree->fall();
-    $appleTree->winter();
+
 }
---
+++ tree_interface.php
@@ -0,0 +1,10 @@
+<?php
+namespace Learning;
+
+interface TreeInterface {
+    function grow();
+    function bloom();
+    function watch();
+    function harvest();
+    function prune();
+}
